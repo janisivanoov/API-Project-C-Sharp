5edcfbc37ffb0a7ea6b5e8577117b076f0a45cbb
Janis <janisivanov@icloud.com>
2021-09-29 11:18:11 +0300

    Deleted useless staff & comments from Startup

--- mysqltest/Startup.cs
+++ mysqltest/Startup.cs
@@ -19,23 +19,17 @@ namespace mysqltest
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             var connectionString = Configuration["ConnectionString:ClubsDb"];
 
-            // Replace with your server version and type.
-            // Use 'MariaDbServerVersion' for MariaDB.
-            // Alternatively, use 'ServerVersion.AutoDetect(connectionString)'.
-            // For common usages, see pull request #1233.
             var serverVersion = new MySqlServerVersion(new Version(8, 0, 25));
 
-            // Replace 'YourDbContext' with the name of your own DbContext derived class.
             services.AddDbContext<ClubsContext>(
                 dbContextOptions => dbContextOptions
                     .UseMySql(connectionString, serverVersion)
-                    .EnableSensitiveDataLogging() // <-- These two calls are optional but help
-                    .EnableDetailedErrors()       // <-- with debugging (remove for production).
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors()
             );
 
             services.AddSingleton(sp => MapperInitializer.GetMapper());
@@ -43,7 +37,6 @@ namespace mysqltest
             services.AddSwaggerGen();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -51,7 +44,6 @@ namespace mysqltest
                 app.UseDeveloperExceptionPage();
             }
 
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger(c =>
             {
                 c.SerializeAsV2 = true;
@@ -65,11 +57,9 @@ namespace mysqltest
             {
                 endpoints.MapControllers();
             });
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
+
             app.UseSwagger();
 
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-            // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
