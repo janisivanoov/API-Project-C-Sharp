e41d4d7af1f6803b860645bc09bb67ba5187411f
Janis <janisivanov@icloud.com>
2021-10-07 15:56:38 +0300

    Deleted useless lines of code

--- mysqltest/Models/Helpers/ClubsContext.cs
+++ mysqltest/Models/Helpers/ClubsContext.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using mysqltest.Enums;
-using System;
 
 namespace mysqltest.Models
 {
@@ -9,51 +7,6 @@ namespace mysqltest.Models
         public ClubsContext(DbContextOptions options)
           : base(options)
         {
-        }
-
-        //TODO: THINK IF ITS NEEDED
-        //TODO: THINK IF ITS NEEDED
-        //TODO: THINK IF ITS NEEDED
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Club>().HasData(
-                new Club() { Id = 1, Name = "International", Type = ClubType.Other, CreatedDate = DateTime.Now, UpdatedDate = DateTime.Now, Status = Enumeration.ClubStatus.Active },
-                new Club() { Id = 2, Name = "Math", Type = ClubType.Academic, CreatedDate = DateTime.Now, UpdatedDate = DateTime.Now, Status = Enumeration.ClubStatus.Unknown },
-                new Club() { Id = 3, Name = "Diving", Type = ClubType.Sport, CreatedDate = DateTime.Now, UpdatedDate = DateTime.Now, Status = Enumeration.ClubStatus.Inactive },
-                new Club() { Id = 4, Name = "Strollers", Type = ClubType.Leisure, CreatedDate = DateTime.Now, UpdatedDate = DateTime.Now, Status = Enumeration.ClubStatus.Active });
-
-            modelBuilder.Entity<Student>().HasData(
-                new Student() { Id = 1, FirstName = "Ali", LastName = "Tayari", BirthDate = new DateTime(1999, 8, 1), CreatedDate = DateTime.Now, UpdatedDate = DateTime.Now },
-                new Student() { Id = 2, FirstName = "Dmitry", LastName = "Apraksin", BirthDate = new DateTime(1963, 8, 9), CreatedDate = DateTime.Now, UpdatedDate = DateTime.Now },
-                new Student() { Id = 3, FirstName = "Ivan", LastName = "Ivanou", BirthDate = new DateTime(2004, 8, 12), CreatedDate = DateTime.Now, UpdatedDate = DateTime.Now },
-                new Student() { Id = 4, FirstName = "Petr", LastName = "Petrov", BirthDate = new DateTime(2000, 1, 1), CreatedDate = DateTime.Now, UpdatedDate = DateTime.Now },
-                new Student() { Id = 5, FirstName = "Sidor", LastName = "Sidorov", BirthDate = new DateTime(1989, 2, 3), CreatedDate = DateTime.Now, UpdatedDate = DateTime.Now },
-                new Student() { Id = 6, FirstName = "Pambos", LastName = "Boss", BirthDate = new DateTime(2000, 5, 10), CreatedDate = DateTime.Now, UpdatedDate = DateTime.Now },
-                new Student() { Id = 7, FirstName = "Christos", LastName = "Christou", BirthDate = new DateTime(1998, 4, 5), CreatedDate = DateTime.Now, UpdatedDate = DateTime.Now },
-                new Student() { Id = 8, FirstName = "Savvas", LastName = "Savvou", BirthDate = new DateTime(1999, 12, 21), CreatedDate = DateTime.Now, UpdatedDate = DateTime.Now }
-          );
-            modelBuilder.Entity<StudentClub>().HasData(
-                new StudentClub() { Id = 1, ClubId = 1, StudentId = 1 },
-                new StudentClub() { Id = 2, ClubId = 2, StudentId = 1 },
-                new StudentClub() { Id = 3, ClubId = 3, StudentId = 2 },
-                new StudentClub() { Id = 4, ClubId = 4, StudentId = 3 },
-                new StudentClub() { Id = 5, ClubId = 3, StudentId = 4 },
-                new StudentClub() { Id = 6, ClubId = 2, StudentId = 5 },
-                new StudentClub() { Id = 7, ClubId = 1, StudentId = 6 },
-                new StudentClub() { Id = 8, ClubId = 2, StudentId = 7 },
-                new StudentClub() { Id = 9, ClubId = 3, StudentId = 8 },
-                new StudentClub() { Id = 10, ClubId = 4, StudentId = 3 },
-                new StudentClub() { Id = 11, ClubId = 2, StudentId = 5 },
-                new StudentClub() { Id = 12, ClubId = 2, StudentId = 6 },
-                new StudentClub() { Id = 13, ClubId = 1, StudentId = 7 }
-                );
-            modelBuilder.Entity<User>().HasData(
-                new User() { FirstName = "Ali", LastName = "Tayari", Id = 1, PasswordHash = "12345", Username = "tester", Password = "12345" },
-                new User() { FirstName = "Ivan", LastName = "Ivanov", Id = 2, PasswordHash = "12345", Username = "tester_2", Password = "12345" });
-            modelBuilder.Entity<VaccinatedUser>().HasData(
-                new VaccinatedUser() { Id = 1, FirstName = "Ivan", LastName = "Ivanou", Date_Of_Birth = new DateTime(2004, 8, 12), Id_Card_Number = 123456789, PhoneNumber = 99633191, VaccinatedStatus = Enumeration.VaccinatedStatus.Vaccinated, VaccinatedType = Enumeration.VaccineType.Pfizer, CreatedDate = DateTime.Now, UpdatedDate = DateTime.Now },
-                new VaccinatedUser() { Id = 2, FirstName = "Ali", LastName = "Tayari", Date_Of_Birth = new DateTime(1999, 8, 1), Id_Card_Number = 987654321, PhoneNumber = 19133699, VaccinatedStatus = Enumeration.VaccinatedStatus.First_dose, VaccinatedType = Enumeration.VaccineType.Moderna, CreatedDate = DateTime.Now, UpdatedDate = DateTime.Now });
         }
 
         public DbSet<Club> Clubs { get; set; }
